4bcebb4473a849fe3127601d1c3edd1ac3a9f1de
Paolo Bosetti <paolo.bosetti@ing.unitn.it>
2015-06-03 11:10:48 +0200

    Generalized constructors

--- src/Kalman.cpp
+++ src/Kalman.cpp
@@ -1,24 +1,41 @@
 #include "Kalman.h"
 
 Kalman::Kalman(){
-  _theta_est   = 0;
-  _thetad_bias_est = 0;
-  _P[0][0]  = 10;
-  _P[1][0]  = 0;
-  _P[0][1]  = 0;
-  _P[1][1]  = 10;
+  double P[2][2];
+  P[0][0] = 10;
+  P[1][0] = 0;
+  P[0][1] = 0;
+  P[1][1] = 10;
+  init(0.0, 0.0, P);
 }
 
-Kalman::Kalman(float theta_est_0, float thetad_bias_est_0, float P_0[2][2]){
-  _theta_est = theta_est_0;
+Kalman::Kalman(double theta_est_0) {
+  double P[2][2];
+  P[0][0] = 10;
+  P[1][0] = 0;
+  P[0][1] = 0;
+  P[1][1] = 10;
+  init(theta_est_0, 0.0, P);
+}
+
+Kalman::Kalman(double theta_est_0, double thetad_bias_est_0, double P_0[2][2]){
+  init(theta_est_0, thetad_bias_est_0, P_0);
+}
+
+void Kalman::init(double theta_est_0, double thetad_bias_est_0, double P_0[2][2]) {
+  _theta_est       = theta_est_0;
   _thetad_bias_est = thetad_bias_est_0;
-  _P[0][0]  = P_0[0][0];
-  _P[1][0]  = P_0[1][0];
-  _P[0][1]  = P_0[0][1];
-  _P[1][1]  = P_0[1][1];
+  _P[0][0]       = P_0[0][0];
+  _P[1][0]       = P_0[1][0];
+  _P[0][1]       = P_0[0][1];
+  _P[1][1]       = P_0[1][1];
+  _Q_theta       = 0.0000001;
+  _Q_thetad_bias = 0.00001;
+  _R             = 2.0;
 }
 
-float Kalman::update(float dt, float theta, float thetad) {
+
+double Kalman::update(double dt, double theta, double thetad) {
   // estimate step
   _theta_est += dt*(thetad - _thetad_bias_est);
   _P[0][0] += dt*(_P[1][1]*dt - _P[0][1] - _P[1][0] + _Q_theta);
--- src/Kalman.h
+++ src/Kalman.h
@@ -5,21 +5,29 @@ class Kalman{
 
   public:
     Kalman();
-    Kalman(float, float, float[2][2]);
-    float update(float, float, float);
+		Kalman(double theta_est_0);
+    Kalman(double theta_est_0, double thetad_bias_est_0, double P_0[2][2]);
+		void init(double theta_est_0, double thetad_bias_est_0, double P_0[2][2]);
+    double update(double, double, double);
+		double theta_est() {return _theta_est;};
+		double Q_theta() {return _Q_theta;};
+		double Q_thetad_bias() {return _Q_thetad_bias;};
+		void set_R(double val) {_R = val;};
+		void set_Q_theta(double val) {_Q_theta = val;};
+		void set_Q_thetad_bias(double val) {_Q_thetad_bias = val;};
   private:
     // PARAMETERS
-    float _Q_theta;        // kalmann filter angle covariance
-    float _Q_thetad_bias;  // kalmann filter angular velocity convariance
-    float _R;              // kalmann filter measure variance
+    double _Q_theta;        // kalmann filter angle covariance
+    double _Q_thetad_bias;  // kalmann filter angular velocity convariance
+    double _R;              // kalmann filter measure variance
     // ESTIMATED VARIABLES
-    float _theta_est;
-    float _thetad_bias_est;
+    double _theta_est;
+    double _thetad_bias_est;
     // INTERNAL FILTER VARIABLES
-    float _P[2][2];
-    float _y;
-    float _S;
-    float _K[2];
+    double _P[2][2];
+    double _y;
+    double _S;
+    double _K[2];
 };
 
 #endif
